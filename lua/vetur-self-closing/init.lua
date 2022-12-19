9a4666ee0a9eaecda957a2c0e5ae47abd7357220
kurageso <63407106+kurageso@users.noreply.github.com>
2022-12-19 22:15:31 +0900

    stylua

--- lua/vetur-self-closing/init.lua
+++ lua/vetur-self-closing/init.lua
@@ -1,24 +1,24 @@
 local M = {}
 
 local function setup()
-vim.api.nvim_create_augroup("vetur-self-closing",  {})
-vim.api.nvim_create_autocmd({"BufReadPost", "FileReadPost"}, {
-  group = 'vetur-self-closing',
-  pattern = {'*.vue'},
-  command = '%s;^<\\(\\S\\{-1,}\\)\\s\\(.*\\)/>;<\\1 \\2></\\1>;eg'
-})
+	vim.api.nvim_create_augroup("vetur-self-closing", {})
+	vim.api.nvim_create_autocmd({ "BufReadPost", "FileReadPost" }, {
+		group = "vetur-self-closing",
+		pattern = { "*.vue" },
+		command = "%s;^<\\(\\S\\{-1,}\\)\\s\\(.*\\)/>;<\\1 \\2></\\1>;eg",
+	})
 
-vim.api.nvim_create_autocmd({"BufEnter"}, {
-  group = "vetur-self-closing",
-  pattern = {"*.vue"},
-  command = '%s;^<\\(\\S\\{-1,}\\)\\s\\(.*\\)/>;<\\1 \\2></\\1>;eg | w'
-})
+	vim.api.nvim_create_autocmd({ "BufEnter" }, {
+		group = "vetur-self-closing",
+		pattern = { "*.vue" },
+		command = "%s;^<\\(\\S\\{-1,}\\)\\s\\(.*\\)/>;<\\1 \\2></\\1>;eg | w",
+	})
 
-vim.api.nvim_create_autocmd({"BufLeave"}, {
-  group = "vetur-self-closing",
-  pattern = {"*.vue"},
-  command = '%s;^<\\(.*\\)></.*>;<\\1/>;eg | w'
-})
+	vim.api.nvim_create_autocmd({ "BufLeave" }, {
+		group = "vetur-self-closing",
+		pattern = { "*.vue" },
+		command = "%s;^<\\(.*\\)></.*>;<\\1/>;eg | w",
+	})
 end
 
 M.setup = setup
